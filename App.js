af2662c5f756b155698b9d4a4ee558187d304ffe
Adrenine <adrenine13@gmail.com>
2021-05-04 15:16:49 +0530

    Added functionality for Assets Caching

--- App.js
+++ App.js
@@ -1,9 +1,56 @@
-//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Providers from "./src/navigation";
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
+//import { StatusBar } from 'expo-status-bar';
 
-export default function App() {
-  return <Providers/>
+//cache images
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+//caching fonts
+// function cacheFonts(fonts) {
+//   return fonts.map(font => Font.loadAsync(font));
+// }
+
+export default class App extends React.Component {
+  state = {
+    isReady: false
+  };
+
+  async _loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/png/search.png'),
+      require('./assets/png/map.png'),
+      require('./assets/png/contact.png'),
+      require('./assets/png/welcome.png')
+    ]);
+
+    // const fontsAssets = cacheFonts([]);
+
+    await Promise.all([...imageAssets])
+  }
+
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onFinish={this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    };
+    return <Providers/>
+  }
 }
 
 // const styles = StyleSheet.create({
